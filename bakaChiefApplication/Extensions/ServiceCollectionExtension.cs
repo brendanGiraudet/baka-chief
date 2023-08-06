e1953606817fa5647ed9115f310ea608197873d6
brendanGiraudet <brendan.giraudet1@gmail.com>
2023-08-06 22:10:40 +0200

    add extensions

---
+++ bakaChiefApplication/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,35 @@
+﻿using bakaChiefApplication.Repositories;
+using bakaChiefApplication.Repositories.NutrimentTypeRepository;
+using bakaChiefApplication.Services.NutrimentTypeService;
+using bakaChiefApplication.ViewModels.NutrimentTypeViewModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace bakaChiefApplication.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<INutrimentTypeRepository, NutrimentTypeRepository>();
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<INutrimentTypeService, NutrimentTypeService>();
+        }
+
+        public static void AddViewModels(this IServiceCollection services)
+        {
+            services.AddTransient<INutrimentTypeViewModel, NutrimentTypeViewModel>();
+        }
+
+        public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+            Console.WriteLine($"cccc {connectionString}");
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlite(connectionString));
+        }
+    }
+}
--- bakaChiefApplication/Program.cs
+++ bakaChiefApplication/Program.cs
@@ -1,4 +1,5 @@
 using bakaChiefApplication;
+using bakaChiefApplication.Extensions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+builder.Services.AddViewModels();
+builder.Services.AddDbContext(builder.Configuration);
 
 await builder.Build().RunAsync();
