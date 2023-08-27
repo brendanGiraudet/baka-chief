aac7e9fc70513db09a762daafae8318283ff5805
brendanGiraudet <brendan.giraudet1@gmail.com>
2023-08-27 22:45:46 +0200

    add button component

---
+++ bakaChiefApplication/Components/Button/Button.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace bakaChiefApplication.Components.Button
+{
+    public partial class Button
+    {
+        [Parameter]
+        public string Text { get; set; }
+        
+        [Parameter]
+        public string Type { get; set; }
+        
+        [Parameter]
+        public EventCallback OnClickCallback { get; set; }
+
+
+        private async Task Onclick()
+        {
+            if(OnClickCallback.HasDelegate) await OnClickCallback.InvokeAsync();
+        }
+    }
+}
