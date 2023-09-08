49c9ec4f9b900b4e3157f318c684a820b5338541
brendanGiraudet <brendan.giraudet1@gmail.com>
2023-09-08 20:57:56 +0200

    fix update ingredients and rename title

--- bakaChiefApplication/Store/Ingredients/IngredientsReducer.cs
+++ bakaChiefApplication/Store/Ingredients/IngredientsReducer.cs
@@ -34,7 +34,7 @@ namespace bakaChiefApplication.Store.Ingredients
         [ReducerMethod]
         public static IngredientsState ReduceRemoveSelectedNutrimentAction(IngredientsState state, RemoveSelectedNutrimentAction action)
         {
-            state.Ingredient.Nutriments = state.Ingredient.Nutriments.Where(n => n.Id == action.Nutriment.Id);
+            state.Ingredient.Nutriments = state.Ingredient.Nutriments.Where(n => n.Id != action.Nutriment.Id);
 
             return new IngredientsState(ingredients: state.Ingredients, ingredient: state.Ingredient, isIngredientFormHidden: false);
         }
