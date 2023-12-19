9cc72f1ec0985d54f6251383a73ce3b68fe9034a
brendanGiraudet <brendan.giraudet1@gmail.com>
2023-12-19 22:56:17 +0100

    add version display

--- bakaChiefApplication/Shared/NavMenu.razor.cs
+++ bakaChiefApplication/Shared/NavMenu.razor.cs
@@ -1,8 +1,20 @@
-﻿namespace bakaChiefApplication.Shared
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Components;
+
+namespace bakaChiefApplication.Shared
 {
     public partial class NavMenu
     {
         private bool _collapseNavMenu = true;
+        private string _applicationVersion = string.Empty;
+
+        [Inject] public IConfiguration Configuration { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            _applicationVersion = Configuration["ApplicationVersion"];
+        }
 
         private void ToggleNavMenu()
         {
