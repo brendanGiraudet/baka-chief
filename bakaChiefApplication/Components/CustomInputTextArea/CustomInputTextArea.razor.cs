1b518043d6b8dfa79e6a35a92fe3b9885259c05b
brendanGiraudet <brendan.giraudet1@gmail.com>
2024-01-28 15:50:48 +0100

    add custominputtextarea and use it for recip preparation

---
+++ bakaChiefApplication/Components/CustomInputTextArea/CustomInputTextArea.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace bakaChiefApplication.Components.CustomInputTextArea
+{
+    public partial class CustomInputTextArea : InputTextArea
+    {
+        [Parameter] public string Label { get; set; }
+    }
+}
