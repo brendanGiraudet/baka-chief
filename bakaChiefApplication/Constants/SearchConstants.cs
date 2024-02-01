1912d9dcf052a53f60d53324b8c88a4f89c90066
brendanGiraudet <brendan.giraudet1@gmail.com>
2024-02-01 22:44:12 +0100

    update searchconstant and set 2.5.1 version

--- bakaChiefApplication/Constants/SearchConstants.cs
+++ bakaChiefApplication/Constants/SearchConstants.cs
@@ -2,9 +2,9 @@ namespace bakaChiefApplication.Constants;
 
 public static class SearchConstants
 {
-    public static int DefaultNumberOfItemsToTake = 1;
+    public static int DefaultNumberOfItemsToTake = 15;
     
     public static int DefaultNumberOfItemsToSkip = 0;
     
-    public static int DefaultNumberOfItemsToTakeInForm = 1;
+    public static int DefaultNumberOfItemsToTakeInForm = 3;
 }
