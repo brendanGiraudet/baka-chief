5ffe0474511044cfbc694d128845854f3027e0b1
brendanGiraudet <brendan.giraudet1@gmail.com>
2024-02-01 21:42:59 +0100

    rename addbutton to flotted button andmove button from page

--- bakaChiefApplication/Components/FlottedButton/FlottedButton.razor.cs
+++ bakaChiefApplication/Components/FlottedButton/FlottedButton.razor.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace bakaChiefApplication.Components.AddButton
+namespace bakaChiefApplication.Components.FlottedButton
 {
-    public partial class AddButton
+    public partial class FlottedButton
     {
         [Parameter] public EventCallback OnClickCallback { get; set; }
+        
+        [Parameter] public string? Text { get; set; }
+
         private async Task Onclick()
         {
             if (OnClickCallback.HasDelegate) await OnClickCallback.InvokeAsync();
