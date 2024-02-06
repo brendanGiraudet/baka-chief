3bf27b96b284e73899552e670878685211db924e
brendanGiraudet <brendan.giraudet1@gmail.com>
2024-02-06 22:22:37 +0100

    use measure unit in recip

--- bakaChiefApplication/Components/RecipIngredient/RecipIngredient.razor.cs
+++ bakaChiefApplication/Components/RecipIngredient/RecipIngredient.razor.cs
@@ -1,14 +1,38 @@
+using bakaChiefApplication.Configurations;
+using bakaChiefApplication.Store.BaseStore.Actions;
+using bakaChiefApplication.Store.MeasureUnits;
+using Fluxor;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
 
 namespace bakaChiefApplication.Components.RecipIngredient;
 
 public partial class RecipIngredient
 {
+    [Inject] public IState<MeasureUnitsState> MeasureUnitsState { get; set; }
+
+    [Inject] public IDispatcher Dispatcher { get; set; }
+
     [Parameter] public Models.RecipIngredient CurrentRecipIngredient { get; set; }
+
     [Parameter] public Enums.Icon Icon { get; set; }
+
     [Parameter] public Enums.Size Size { get; set; }
+
     [Parameter] public Enums.Style Style { get; set; }
+
     [Parameter] public EventCallback OnIconClickCallback { get; set; }
+
+    [Inject] public IOptions<SearchConfiguration> SearchConfigurationOptions { get; set; }
+
+    SearchConfiguration SearchConfiguration => SearchConfigurationOptions.Value;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Dispatcher.Dispatch(new SearchByNameAction<Models.MeasureUnit>(string.Empty, take: SearchConfiguration.MaxNumberOfItemsToTake));
+    }
 
     private async Task OnIconClick()
     {
